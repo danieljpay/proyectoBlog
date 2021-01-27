d79d45f4f2eaf58ad394d3c2c9a392cab556ba16
alextorrer <alextorre0427@gmail.com>
2021-01-27 16:52:50 -0600

    Traer publicaciones del CMS

--- application/controller/inicio.php
+++ application/controller/inicio.php
@@ -1,13 +1,39 @@
 <?php
 
 class Inicio extends Controller{
+
     public $MODULE = "inicio";
     public $TITLE = "Home";
+    public $posts = array();
 
     public function index(){
+        $this->getAllPosts();
         require APP . 'view/_templates/header.php';
         require APP . 'view/inicio/inicio.php';
         require APP . 'view/_templates/footer.php';
     }
 
+    private function getAllPosts(){
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => URL_CMS.'/api/collections/get/posts',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>'{
+                "filter": {
+                    "published" : true
+                }
+            }',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer 43a0dc3c3900d5677bab77e58e05ed',
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        
+        $json = json_decode($response);
+        $this->posts = $json->entries;
+    }
 }
--- application/view/inicio/inicio.php
+++ application/view/inicio/inicio.php
@@ -2,30 +2,20 @@
     <div>
         <h1>Últimas publicaciones</h1>
         <div class="container">
-            <article>
-                <img src="<?php echo URL;?>img/image.png" alt="">
-                <div>
-                    <h3>Titulo de la publicación</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus neque voluptatibus error non nesciunt impedit rem ullam eius velit natus inventore, minima consequatur eum molestias, doloremque porro eveniet ratione animi!</p>
-                    <a href="<?php echo URL; ?>post">Ver publicación...</a>
-                </div>
-            </article>
-            <article>
-                <img src="<?php echo URL;?>img/image.png" alt="">
-                <div>
-                    <h3>Titulo de la publicación</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus neque voluptatibus error non nesciunt impedit rem ullam eius velit natus inventore, minima consequatur eum molestias, doloremque porro eveniet ratione animi!</p>
-                    <a href="<?php echo URL; ?>post">Ver publicación...</a>
-                </div>
-            </article>
-            <article>
-                <img src="<?php echo URL;?>img/image.png" alt="">
-                <div>
-                    <h3>Titulo de la publicación</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus neque voluptatibus error non nesciunt impedit rem ullam eius velit natus inventore, minima consequatur eum molestias, doloremque porro eveniet ratione animi!</p>
-                    <a href="<?php echo URL; ?>post">Ver publicación...</a>
-                </div>
-            </article>
+            <?php 
+                foreach($this->posts as $post){
+                    echo '
+                        <article>
+                            <img src="'.URL_PROTOCOL.URL_CMS.'/storage/uploads'.$post->image->path.'" alt="">
+                            <div>
+                                <h3>'.$post->title.'</h3>
+                                <p>'.$post->description.'</p>
+                                <a href="<?php echo URL; ?>post">Ver publicación...</a>
+                            </div>
+                        </article>
+                    ';
+                }
+            ?>
         </div>
     </div>
 </main>
