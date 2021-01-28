47e05331fab21c5d99f17c425f1b647ec94316e2
alextorrer <alextorre0427@gmail.com>
2021-01-27 21:42:28 -0600

    Implementar busqueda

--- application/controller/buscar.php
+++ application/controller/buscar.php
@@ -6,6 +6,29 @@ class Buscar extends Controller{
     public $TITLE = "Resultados de tu búsqueda";
 
     public function index(){
+        $query = $_GET["query"];
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => URL_CMS.'/api/collections/get/posts',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>'{
+                "filter": {
+                    "title" : "'.$query.'"
+                }
+            }',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer 43a0dc3c3900d5677bab77e58e05ed',
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        
+        $json = json_decode($response);
+        $posts = $json->entries;
+
         require APP . 'view/_templates/header.php';
         require APP . 'view/busqueda/busqueda.php';
         require APP . 'view/_templates/footer.php';
--- application/view/busqueda/busqueda.php
+++ application/view/busqueda/busqueda.php
@@ -1,35 +1,25 @@
 <main>
-    <h2>Resultados que coinciden con tu búsqueda</h2>
-    <div class="container">
-        <article>
-            <div class="img-container">
-                <img src="<?php echo URL;?>img/image.png" alt="">
-            </div>
-            <div class="post-container">
-                <h3>Titulo de la publicación</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus neque voluptatibus error non nesciunt impedit rem ullam eius velit natus inventore, minima consequatur eum molestias, doloremque porro eveniet ratione animi!</p>
-                <a href="<?php echo URL; ?>post">Ver publicación...</a>
-            </div>
-        </article>
-        <article>
-            <div class="img-container">
-                <img src="<?php echo URL;?>img/image.png" alt="">
-            </div>
-            <div class="post-container">
-                <h3>Titulo de la publicación</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus neque voluptatibus error non nesciunt impedit rem ullam eius velit natus inventore, minima consequatur eum molestias, doloremque porro eveniet ratione animi!</p>
-                <a href="<?php echo URL; ?>post">Ver publicación...</a>
-            </div>
-        </article>
-        <article>
-            <div class="img-container">
-                <img src="<?php echo URL;?>img/image.png" alt="">
-            </div>
-            <div class="post-container">
-                <h3>Titulo de la publicación</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus neque voluptatibus error non nesciunt impedit rem ullam eius velit natus inventore, minima consequatur eum molestias, doloremque porro eveniet ratione animi!</p>
-                <a href="<?php echo URL; ?>post">Ver publicación...</a>
-            </div>
-        </article>
+    <?php
+        if(isset($posts)){
+            echo '<h2>Resultados que coinciden con tu búsqueda: "'.$query.'"</h2>';
+            echo '<div class="container">';
+            
+            foreach($posts as $post){
+                echo '
+                    <article>
+                        <div class="img-container">
+                            <img src="'.URL_PROTOCOL.URL_CMS.'/storage/uploads'.$post->image->path.'" alt="">
+                        </div>
+                        <div class="post-container">
+                            <h3>'.$post->title.'</h3>
+                            <p>'.$post->description.'</p>
+                            <a href="'.URL.'post">Ver publicación...</a>
+                        </div>
+                    </article>
+                ';
+            }
+
+        }
+    ?>
     </div>
 </main>
