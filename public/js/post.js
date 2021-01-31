9c5c9609c22f763a2495deae805f2bcf2ba36e1d
JimmyOjedaUADY <a15001775@alumnos.uady.mx>
2021-01-30 18:20:04 -0600

    Comentarios por JS restaurados. Remover etiquetas HTML/JS/PHP en comentarios implementado.

--- public/js/post.js
+++ public/js/post.js
@@ -9,3 +9,37 @@ printPostButton.addEventListener('click', () => {
     w.print();
     w.close();
 })
+
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth()+1;
+var year = date.getFullYear();
+
+function addComment (){
+
+    var commentContainer = document.getElementById("c_cont");
+    var comment = document.getElementById("commentAreaInvisible");
+    var fakeTextArea = document.getElementById("fakeTextArea");
+    
+    if (comment.textContent.length > 0) {
+        commentContainer.innerHTML += '<div>' + 
+        '<div class="perfil">' +
+            '<img src="../../public/img/userAnonimus.jpg" alt="">' +
+            '<label for="">Usuario anonimo</label>' +
+        '</div>' +
+        '<div class="comment_containt">' +
+            '<p>' + sanitizeText(comment.textContent) + '</p>' +
+            '<label for="">' + day + '/' + month + '/' + year + '</label>' +
+        '</div>' +
+        '</div>'
+        comment.textContent = "";
+        fakeTextArea.textContent  = "";
+    }
+};
+
+function sanitizeText (comment){
+    let str = comment;
+    let stripped = str.replace(/(<([^>]+)>)/ig, ''); // Texto de prueba
+    return stripped;
+}
+
